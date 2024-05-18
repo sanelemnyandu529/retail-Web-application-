65419e0484cf149a2dc8d13f384830e5f4cbdd80
sanelemnyandu529 <143176260+sanelemnyandu529@users.noreply.github.com>
2024-05-19 00:49:21 +0200

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ImageTest](
+ [ID] [int] IDENTITY(1,1) NOT NULL,
+ [Image] [varbinary](MAX) NULL
+ ) ON [PRIMARY]
+ GO
+
+ --Insert Image
+ INSERT INTO [dbo].[ImageTest]([Image])
+ SELECT * FROM
+OPENROWSET(BULK 'C:\Users\sanel\Documents\BusinesssTrinitySP01\BusinesssTrinitySP01\Content\images\a3.jpg', SINGLE_BLOB) AS Document
+ GO
---
+++ SQLQuery3.sql
@@ -0,0 +1,10 @@
+﻿UPDATE Equipments
+SET
+    Image1 = (SELECT BulkColumn FROM OPENROWSET(BULK 'C:\Users\sanel\Documents\BusinesssTrinitySP01\BusinesssTrinitySP01\Content\TrinityImages\Sugar5kg.jpg', SINGLE_BLOB) AS Document),
+    Image2 = (SELECT BulkColumn FROM OPENROWSET(BULK 'C:\Users\sanel\Documents\BusinesssTrinitySP01\BusinesssTrinitySP01\Content\TrinityImages\Sugar5kg.jpg', SINGLE_BLOB) AS Document),
+    Image3 = (SELECT BulkColumn FROM OPENROWSET(BULK 'C:\Users\sanel\Documents\BusinesssTrinitySP01\BusinesssTrinitySP01\Content\TrinityImages\Sugar5kg.jpg', SINGLE_BLOB) AS Document),
+    Name='Selati Sugar',
+    Rentprice=45,
+    Description='Selati Brown Sugar (1 X 5kg)'
+
+WHERE EquipmentID = 9;
